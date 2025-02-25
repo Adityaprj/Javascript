5659b2b7d9cf0ec8618d651899ff3f7290452387
Aditya Kumar <araj22073@gmail.com>
2025-02-25 21:15:18 +0530

    js

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ console.info("hello world!");
 console.warn("hello world!");
 console.error("hello world!");
 console.group("hello world!");
+
+// for swap values
+var a=1;
+var b=2;
+var c;
+
+c=a;
+a=b;
+b=c;
+console.log(a,b);
+// by destructuring
+
+var a=1;
+var b=2;
+var c;
+[a,b]=[b,a];
+console.log(a,b);
+
+// by addition and subtraction
+
+var a=11;
+var b=12;
+a+=b;
+b=a-b;
+a=a-b;
+console.log(a,b);
