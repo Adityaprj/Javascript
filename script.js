0a2705a864030ee36ca705f5e6d2e94d1a6a4d95
Aditya Kumar <araj22073@gmail.com>
2025-02-25 21:47:25 +0530

    js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+// for printing hello by ,multiple method.
 console.log("hello world!");
 console.info("hello world!");
 console.warn("hello world!");
 console.error("hello world!");
 // group
 console.group("pehla dein");
-console.log("das");
-console.log("s");
-console.groupEnd();
+    console.log("das");
+    console.log("s");
+    console.groupEnd();
+
+console.groupCollapsed("github data");
+    console.log("add");
+    console.log("commit");
+    console.log("push");
+    console.log("code pushed to github");
+    console.groupEnd();
 
 // for swap values
 var a=1;
@@ -33,3 +41,39 @@ a+=b;
 b=a-b;
 a=a-b;
 console.log(a,b);
+
+// decalring a const and modifying its properties
+
+const person = {
+    name: "sam",
+    age: 21
+};
+person.name = "john";
+console.log(person);
+// we can update the value of const but can not change the value of const.
+// values are cannot be set again.
+// and if we want to make its value unchangeble then we can use Object.freeze() method.
+
+// convert string to number 
+Number("50")
+parseInt("50")
++50
+
+// find anything without using includes
+
+let str = "tmkc";
+
+if (str.indexOf("m") !== -1){
+    console.log("found");
+}
+else{
+    console.log("not found");
+}
+
+// also you can put .search in place of indexOf
+// ===(equal)  !==(not equal)  ==(equal)  !=(not equal)
+
+// difference between undefined and null,NaN
+var a; // undefined
+var 1/0; // NaN
+var c = null; // null
